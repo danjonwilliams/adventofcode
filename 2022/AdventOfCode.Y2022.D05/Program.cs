0f11441f856a8bfe1fc945a87bef543231eb4436
Dan Williams <dan.jon.williams@gmail.com>
2022-12-27 11:24:27 +0000

    Day 6

---
+++ 2022/AdventOfCode.Y2022.D05/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace AdventOfCode.Y2022.D05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt").Skip(10).ToArray();
+            // Hardcode starting stacks for convenience
+            var stacks = new Stack<char>[]
+                {
+                    new Stack<char>(new[] { 'L', 'N', 'W', 'T', 'D' }),
+                    new Stack<char>(new[] {'C', 'P', 'H' }),
+                    new Stack<char>(new[] {'W','P','H','N','D','G','M','J'}),
+                    new Stack<char>(new[] {'C', 'W', 'S', 'N', 'T', 'Q', 'L' }),
+                    new Stack<char>(new[] {'P' ,'H', 'C', 'N' }),
+                    new Stack<char>(new[] { 'T','H','N','D','M','W','Q','B'}),
+                    new Stack<char>(new[] {'M','B','R','J','G','S','L' }),
+                    new Stack<char>(new[] { 'Z','N','W','G','V','B','R','T'}),
+                    new Stack<char>(new[] {'W','G','D','N','P','L' }),
+                };
+            var moves = input.Select(i => new Move(i)).ToArray();
+
+            foreach (var move in moves)
+            {
+                ApplyMove9000(stacks, move);
+            }
+            var topCrates = stacks.Select(s => s.Peek()).ToArray();
+            Console.WriteLine($"Part One: Top Crates: {string.Join("", topCrates)}");
+
+            stacks = new Stack<char>[]
+                {
+                    new Stack<char>(new[] { 'L', 'N', 'W', 'T', 'D' }),
+                    new Stack<char>(new[] {'C', 'P', 'H' }),
+                    new Stack<char>(new[] {'W','P','H','N','D','G','M','J'}),
+                    new Stack<char>(new[] {'C', 'W', 'S', 'N', 'T', 'Q', 'L' }),
+                    new Stack<char>(new[] {'P' ,'H', 'C', 'N' }),
+                    new Stack<char>(new[] { 'T','H','N','D','M','W','Q','B'}),
+                    new Stack<char>(new[] {'M','B','R','J','G','S','L' }),
+                    new Stack<char>(new[] { 'Z','N','W','G','V','B','R','T'}),
+                    new Stack<char>(new[] {'W','G','D','N','P','L' }),
+                };
+
+            foreach (var move in moves)
+            {
+                ApplyMove9001(stacks, move);
+            }
+            topCrates = stacks.Select(s => s.Peek()).ToArray();
+            Console.WriteLine($"Part Two: Top Crates: {string.Join("", topCrates)}");
+        }
+
+        private static void ApplyMove9000(Stack<char>[] stacks, Move move)
+        {
+            for (int i = 0; i < move.Amount; i++)
+            {
+                char crate = stacks[move.From].Pop();
+                stacks[move.To].Push(crate);
+            }
+        }
+        private static void ApplyMove9001(Stack<char>[] stacks, Move move)
+        {
+            var tempStack = new Stack<char>();
+            for (int i = 0; i < move.Amount; i++)
+            {
+                char crate = stacks[move.From].Pop();
+                tempStack.Push(crate);
+            }
+            while (tempStack.Count > 0)
+            {
+                char crate = tempStack.Pop();
+                stacks[move.To].Push(crate);
+            }
+        }
+    }
+
+    internal record Move
+    {
+        public int Amount { get; init; }
+        public int From { get; init; }
+        public int To { get; init; }
+
+        public Move(string definition)
+        {
+            var steps = definition.Split(" ");
+            Amount = int.Parse(steps[1]);
+            From = int.Parse(steps[3])-1;
+            To = int.Parse(steps[5])-1;
+        }
+    }
+}
---
+++ 2022/AdventOfCode.Y2022.D06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AdventOfCode.Y2022.D06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = "nppdvjthqldpwncqszvftbrmjlhg";
+            var input = File.ReadAllText("input.txt");
+
+            const int startOfSignalLength = 4;
+            var startOfSignalOffset = GetMarkerPosition(input, startOfSignalLength);
+            Console.WriteLine($"Part One: End of first signal marker: {startOfSignalOffset}");
+
+            const int startOfMessageLength = 14;
+            var startOfMessageOffset = GetMarkerPosition(input, startOfMessageLength);
+            Console.WriteLine($"Part Two: End of first message marker: {startOfMessageOffset}");
+        }
+
+        static int GetMarkerPosition(string dataStream, int markerLength)
+        {
+            var buffer = new LinkedList<char>();
+            for (int readPosition = 0; readPosition < dataStream.Length; readPosition++)
+            {
+                buffer.AddFirst(dataStream[readPosition]);
+                if (buffer.Count() > markerLength)
+                {
+                    buffer.RemoveLast();
+                }
+                var set = new HashSet<char>(buffer);
+                if (set.Count == markerLength)
+                {
+                    return readPosition + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
