454ffd1570f4437a2dff04541a925211cf4865e9
Dan Williams <dan.jon.williams@gmail.com>
2022-12-30 14:39:37 +0000

    Day 12

---
+++ 2022/AdventOfCode.Y2022.D11/Program.cs
@@ -0,0 +1,175 @@
+﻿/* Hacky answer requires changes for each part */
+
+namespace AdventOfCode.Y2022.D11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Monkey[] allMonkeys = null!; // This must be set before acting
+            void ThrowItem(int monkey, Item item)
+            {
+                allMonkeys![monkey].AddItem(item);
+            }
+
+            var input = File.ReadAllLines("input.txt");
+
+            var monkeys = new List<Monkey>();
+            List<List<Item>> monkeyItems = new List<List<Item>>();
+            int inputCount = 0;
+            int worryFactor = 1;
+            while (inputCount <= input.Length)
+            {
+                var itemList = new List<Item>();
+                monkeyItems.Add(itemList);
+                int monkeyNumber = GetMonkeyNumber(input[inputCount++]);
+                itemList.AddRange(GetStartingItems(input[inputCount++]));
+                var interestFactor = GetInterestFactor(input[inputCount++]);
+                var testValue = GetTestValue(input[inputCount++]);
+                worryFactor *= testValue;
+                var trueMonkey = GetTargetMonkey(input[inputCount++]);
+                var falseMonkey = GetTargetMonkey(input[inputCount++]);
+                //skip blank
+                inputCount++;
+                var monkey = new Monkey(interestFactor, testValue, trueMonkey, falseMonkey, ThrowItem);
+                foreach (var item in itemList)
+                {
+                    monkey.AddItem(item);
+                }
+                monkeys.Add(monkey);
+            }
+            allMonkeys = monkeys.ToArray(); // Done it
+            foreach (var item in monkeyItems.SelectMany(x => x))
+            {
+                item.SetWorryFactor(worryFactor);
+            }
+            // Part One
+            //const int maxRounds = 20;
+            // Part Two
+            const int maxRounds = 10000;
+            for (int round = 0; round < maxRounds; round++)
+            {
+                foreach (var monkey in allMonkeys)
+                {
+                    monkey.InspectItems();
+                }
+            }
+
+            var topMonkeys = allMonkeys.OrderByDescending(m => m.InspectionCount).Take(2).ToArray();
+            Console.WriteLine(value: $"Part One: Top Monkeys Factor: {(long)topMonkeys[0].InspectionCount * (long)topMonkeys[1].InspectionCount}");
+        }
+
+        private static int GetTargetMonkey(string line)
+        {
+            return int.Parse(line.Trim().Split(" ")[5]);
+        }
+
+        private static int GetTestValue(string line)
+        {
+            return int.Parse(line.Trim().Split(" ")[3]);
+        }
+
+        private static Func<long, long> GetInterestFactor(string line)
+        {
+            var isMultiply = line.Contains("*");
+            var factor = line.Trim().Split(" ")[5];
+            var isOld = factor.Equals("old");
+
+            if (isOld)
+            {
+                if (isMultiply)
+                {
+                    return x => x * x;
+                }
+                return x => x + x;
+            }
+            var factorValue = int.Parse(factor);
+            if (isMultiply)
+            {
+                return x => x * factorValue;
+            }
+            return x => x + factorValue;
+        }
+
+        private static Item[] GetStartingItems(string line)
+        {
+            var items = line.Trim().Split(":")[1];
+            return items.Split(",").Select(i => new Item(int.Parse(i))).ToArray();
+        }
+
+        private static int GetMonkeyNumber(string line)
+        {
+            return int.Parse(line.Trim().Split(" ")[1][..^1]);
+        }
+    }
+
+    internal class Item
+    {
+        private int _worryFactor;
+
+        public Item(long worryLevel)
+        {
+            WorryLevel = worryLevel;
+        }
+        public long WorryLevel { get; set; }
+
+        public void Relax()
+        {
+            // Part One
+            // WorryLevel /= 3;
+            // Part Two
+            WorryLevel %= _worryFactor;
+        }
+
+        internal void SetWorryFactor(int worryFactor)
+        {
+            _worryFactor = worryFactor;
+        }
+    }
+
+    internal class Monkey
+    {
+        private readonly List<Item> _items = new List<Item>();
+        private readonly Func<long, long> _interestFactor;
+        private readonly int _testMod;
+        private readonly int _trueMonkey;
+        private readonly int _falseMonkey;
+        private readonly Action<int, Item> _throwItem;
+        
+        public int InspectionCount { get; private set; }
+
+        public Monkey(Func<long, long> interestFactor, int testMod, int trueMonkey, int falseMonkey, Action<int, Item> throwItem)
+        {
+            _interestFactor = interestFactor;
+            _testMod = testMod;
+            _trueMonkey = trueMonkey;
+            _falseMonkey = falseMonkey;
+            _throwItem = throwItem;
+        }
+
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void InspectItems()
+        {
+            foreach (Item item in _items)
+            {
+                InspectionCount++;
+                item.WorryLevel = _interestFactor(item.WorryLevel);
+                item.Relax();
+                TestItem(item);
+            }
+            _items.Clear();
+        }
+
+        private void TestItem(Item item)
+        {
+            var targetMonkey = item.WorryLevel % _testMod == 0
+                ? _trueMonkey
+                : _falseMonkey;
+            _throwItem(targetMonkey,item);
+        }
+    }
+}
---
+++ 2022/AdventOfCode.Y2022.D12/Program.cs
@@ -0,0 +1,214 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Y2022.D12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("testinput.txt");
+
+            var map = new Map(input);
+            var startLocation = map.GetStartLocation();
+
+            var distanceTravelled = FindHighestPoint(map, new[] { startLocation });
+            Console.WriteLine($"Part One: Distance travelled : {distanceTravelled}");
+
+            var lowestElevations = map.GetLocations('a').ToArray();
+            var shortestPossible = FindHighestPoint(map, lowestElevations);
+            Console.WriteLine($"Part Two: Shorted distance travelled : {shortestPossible}");
+        }
+
+        private static int FindHighestPoint(Map map, Location[] startLocations)
+        {
+            var currentLocations = new List<Location>(startLocations);
+            var nextLocations = new List<Location>();
+            var processedLocations = new HashSet<Location>();
+            int stepsTaken = 0;
+            bool endReached = false;
+            while (!endReached  && currentLocations.Any())
+            {
+                foreach (var location in currentLocations)
+                {
+                    foreach (var nextMove in map.GetPossibleMoves(location))
+                    {
+                        // If we've been here already, skip
+                        if (processedLocations.Contains(nextMove))
+                        {
+                            continue;
+                        }
+
+                        // This is a possible contender, so move to that space
+                        processedLocations.Add(nextMove);
+                        nextLocations.Add(nextMove);
+                        if (nextMove.IsEnd)
+                        {
+                            endReached = true;
+                            return stepsTaken + 1;
+                        }
+                    }
+                }
+                var tmp = currentLocations;
+                currentLocations = nextLocations;
+                nextLocations = tmp;
+                nextLocations.Clear();
+                stepsTaken++;
+            }
+            return -1;
+        }
+
+        internal class Location
+        {
+            internal static readonly Location Null = new Location(-1, -1, 'Z');
+
+            public Location(int x, int y, char altitude)
+            {
+                X = x;
+                Y = y;
+                if (altitude == 'S')
+                {
+                    Altitude = 'a';
+                    IsStart = true;
+                }
+                else if (altitude == 'E')
+                {
+                    Altitude = 'z';
+                    IsEnd = true;
+                }
+                else
+                {
+                    Altitude = altitude;
+                }
+            }
+
+            public int X { get; private set; }
+            public int Y { get; private set; }
+            public char Altitude { get; private set; }
+            public bool IsStart { get; private set; }
+            public bool IsEnd { get; private set; }
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Location location &&
+                       X == location.X &&
+                       Y == location.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(X, Y);
+            }
+        }
+
+        internal class Map
+        {
+            private readonly Location[,] _map;
+
+            public Map(string[] mapDefinition)
+            {
+                var sizeX = mapDefinition[0].Length;
+                var sizeY = mapDefinition.Length;
+                _map = new Location[sizeX, sizeY];
+                int x = 0;
+                int y = 0;
+                foreach (var mapLine in mapDefinition)
+                {
+                    foreach (var altitude in mapLine)
+                    {
+                        _map[x, y] = new Location(x, y, altitude);
+                        x++;
+                    }
+                    y++;
+                    x = 0;
+                }
+            }
+
+            public Location GetStartLocation()
+            {
+                for (int x = 0; x < _map.GetLength(0); x++)
+                {
+                    for (int y = 0; y < _map.GetLength(1); y++)
+                    {
+                        if (_map[x, y].IsStart)
+                        {
+                            return _map[x, y];
+                        }
+                    }
+                }
+                throw new InvalidOperationException("Map does not have start location");
+            }
+
+            public IEnumerable<Location> GetPossibleMoves(Location currentLocation)
+            {
+                var x = currentLocation.X - 1;
+                var y = currentLocation.Y;
+                Location possibleMove;
+                if (TryGetLocation(x, y, out possibleMove) && IsValidMove(currentLocation, possibleMove))
+                {
+                    yield return possibleMove;
+                }
+                x = currentLocation.X + 1;
+                if (TryGetLocation(x, y, out possibleMove) && IsValidMove(currentLocation, possibleMove))
+                {
+                    yield return possibleMove;
+                }
+                x = currentLocation.X;
+                y = currentLocation.Y - 1;
+                if (TryGetLocation(x, y, out possibleMove) && IsValidMove(currentLocation, possibleMove))
+                {   
+                    yield return possibleMove;
+                }
+                y = currentLocation.Y + 1;
+                if (TryGetLocation(x, y, out possibleMove) && IsValidMove(currentLocation, possibleMove))
+                {
+                    yield return possibleMove;
+                }
+            }
+
+            public bool TryGetLocation(int x, int y, out Location location)
+            {
+                if (x < 0 || y < 0 || x >= _map.GetLength(0) || y >= _map.GetLength(1))
+                {
+                    location = Location.Null;
+                    return false;
+                }
+                location = _map[x, y];
+                return true;
+            }
+
+            public IEnumerable<Location> GetLocations(char altitude)
+            {
+                for (int x = 0; x < _map.GetLength(0); x++)
+                {
+                    for (int y = 0; y < _map.GetLength(1); y++)
+                    {
+                        if (_map[x, y].Altitude == altitude)
+                        {
+                            yield return _map[x, y];
+                        }
+                    }
+                }
+            }
+
+            public string PrintMap()
+            {
+                var sb = new StringBuilder();
+
+                for (int y = 0; y < _map.GetLength(1); y++)
+                {
+                    for (int x = 0; x < _map.GetLength(0); x++)
+                    {
+                        sb.Append(_map[x, y]);
+                    }
+                    sb.AppendLine("");
+                }
+                return sb.ToString();
+            }
+
+            private static bool IsValidMove(Location currentLocation, Location nextLocation)
+            {
+                return nextLocation.Altitude <= currentLocation.Altitude + 1;
+            }
+        }
+    }
+}
